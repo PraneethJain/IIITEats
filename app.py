17745d2e870fc34ee00ed71c72e30a85eab445a4
PraneethJain <praneethjain005@gmail.com>
2023-05-06 18:27:29 +0530

    navbar

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, send_from_directory
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vdn.microsoft.icon",
+    )
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
