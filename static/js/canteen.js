2001610e1fef75165e0d567618976a45345ab138
PraneethJain <praneethjain005@gmail.com>
2023-05-06 22:28:06 +0530

    calculating price

---
+++ static/js/canteen.js
@@ -0,0 +1,31 @@
+const items = document.querySelectorAll(".item");
+const form = document.querySelector("form");
+const travelCost = form.querySelector(".travel-cost");
+const foodCost = form.querySelector(".food-cost");
+const totalCost = form.querySelector(".total-cost");
+
+const getFoodCost = () => {
+  let res = 0;
+  items.forEach((item) => {
+    res +=
+      item.querySelector("select").value *
+      item.querySelector(".right").dataset.price;
+  });
+
+  return res;
+};
+
+const updateTotalCost = () => {
+  const travelCostInt = 40;
+  const foodCostInt = getFoodCost();
+
+  travelCost.textContent = `${travelCostInt}`;
+  foodCost.textContent = `${foodCostInt}`;
+  totalCost.textContent = `${travelCostInt + foodCostInt}`;
+};
+
+items.forEach((item) => {
+  item.querySelector("select").addEventListener("change", () => {
+    updateTotalCost();
+  });
+});
