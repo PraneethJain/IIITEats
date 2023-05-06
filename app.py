d486250d22001064839b00b447837e161561b67b
PraneethJain <praneethjain005@gmail.com>
2023-05-06 22:36:59 +0530

    populated canteen

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, send_from_directory
+import json
 import os
 
 app = Flask(__name__)
@@ -20,10 +21,14 @@ def index() -> str:
 
 @app.route("/canteen/<name>")
 def canteen(name: str) -> str:
-    menu_list = [
-        {"Sandwich": {"Veg Sandwich": 40, "Cheese Potato Sandwich": 50}},
-        {"Burger": {"Veg Burger": 60, "Cheese Burger": 80}},
-    ]
+    with app.open_resource(f"static/menu/{name}.json", "r") as f:
+        menu_list = json.load(f)
+
+    print(menu_list)
+    # menu_list = [
+    #     {"Sandwich": {"Veg Sandwich": 40, "Cheese Potato Sandwich": 50}},
+    #     {"Burger": {"Veg Burger": 60, "Cheese Burger": 80}},
+    # ]
     return render_template("canteen.html", name=name, menu_list=menu_list)
 
 
